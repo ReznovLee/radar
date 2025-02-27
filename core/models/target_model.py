5ecf487a1fa28af0de0aa1e1bba7e153203f81c3
reznovlee <reznovlee@gmail.com>
2025-02-27 12:06:37 +0800

    add new file and modify

--- core/models/target_model.py
+++ core/models/target_model.py
@@ -85,7 +85,8 @@ class BallisticMissileTargetModel(TargetModel):
     """
 
     PRIORITY = 1
-    GRAVITY = np.array([0, 0, 9.81])
+    GRAVITY = np.array([0, 0, -9.81])
+    AIR_RESISTANCE_COEF = 0.5
 
     def __init__(self, target_id, target_position, velocity_mach):
         """ Initializes the target model.
@@ -99,6 +100,20 @@ class BallisticMissileTargetModel(TargetModel):
         :param velocity_mach: Mach is the speed in units
         """
         super().__init__(target_id, target_position, velocity_mach, "Ballistic_Missile", self.PRIORITY)
+        self.acceleration = np.zeros(3)
+
+    def _calculate_air_resistance(self):
+        """ Calculate air resistance.
+
+        The air resistance is calculated based on the current velocity of the target.
+
+        :return: Air resistance
+        """
+        velocity_magnitude = np.linalg.norm(self.velocity_ms)
+        if velocity_magnitude > 0:
+            resistance = -self.AIR_RESISTANCE_COEF * velocity_magnitude * self.velocity_ms
+            return resistance
+        return np.zeros(3)
 
     def update_position(self, time_step):
         """Updates the target position based on the time step.
@@ -108,8 +123,14 @@ class BallisticMissileTargetModel(TargetModel):
         :param time_step: Time step
         """
         self.velocity_ms = self.velocity_mach * MACH_2_MS
-        self.velocity_ms += self.GRAVITY * time_step
+
+        air_resistance = self._calculate_air_resistance()
+        self.acceleration = self.GRAVITY + air_resistance
+
+        self.velocity_ms += self.acceleration * time_step
         self.target_position += self.velocity_ms * time_step
+
+        self.velocity_mach = self.velocity_ms / MACH_2_MS
 
 
 class CruiseMissileTargetModel(TargetModel):
@@ -132,6 +153,8 @@ class CruiseMissileTargetModel(TargetModel):
     CRUISE_ALTITUDE = 8000
     GRAVITY = np.array([0, 0, 9.81])
     TRANSITION_DISTANCE = 500
+    AIR_RESISTANCE_COEF = 0.2
+    DISTURBANCE_SCALE = 0.8
 
     def __init__(self, target_id, target_position, velocity_mach, cruise_end_point, dive_time, cruise_time,
                  rocket_acceleration):
@@ -148,23 +171,53 @@ class CruiseMissileTargetModel(TargetModel):
         """
         super().__init__(target_id, target_position, velocity_mach, "cruise_missile", self.PRIORITY)
         self.current_phase = "cruise"
-        self._disturbance_cache = np.zeros(2)
         self.cruise_end_point = np.array(cruise_end_point)
         self.dive_time = dive_time
         self.cruise_time = cruise_time
         self.rocket_acceleration = rocket_acceleration
+        self.acceleration = np.zeros(3)
 
-    def _apply_disturbance(self, time_step):
-        """ Apply disturbance to the cruise phase.
+    def _calculate_air_resistance(self):
+        """ Calculate air resistance.
+
+        The air resistance is calculated based on the current velocity of the target.
+
+        :return: Air resistance
+        """
+        velocity_magnitude = np.linalg.norm(self.velocity_ms)
+        if velocity_magnitude > 0:
+            resistance = -self.AIR_RESISTANCE_COEF * velocity_magnitude * self.velocity_ms
+            return resistance
+        return np.zeros(3)
+
+    def _apply_cruise_control(self):
+        """ Apply cruise control to the cruise phase.
 
         Cruise missile needs to add disturbance in both cruise and reentry phase to meet the actual operational
         requirements.
-
-        :param time_step: Time step
         """
-        self._disturbance_cache = np.random.normal(0, 0.8, 2) * time_step
-        self.velocity_ms[:2] += self._disturbance_cache
-        self.target_position[2] = self.CRUISE_ALTITUDE
+        height_error = self.CRUISE_ALTITUDE - self.target_position[2]
+        height_correction = np.array([0, 0, height_error * 0.1])
+
+        horizontal_disturbance = np.random.normal(0, self.DISTURBANCE_SCALE, 2)
+        disturbance = np.array([horizontal_disturbance[0], horizontal_disturbance[1], 0])
+
+        return height_correction + disturbance
+
+    def _apply_dive_control(self):
+        """ Apply dive control to the cruise phase.
+
+        Cruise missile needs to add disturbance in both cruise and reentry phase to meet the actual operational
+        requirements.
+        """
+        direction_to_target = self.cruise_end_point - self.target_position
+        distance = np.linalg.norm(direction_to_target)
+        if distance > 0:
+            direction_to_target = direction_to_target / distance
+
+        dive_acceleration = (self.rocket_acceleration * direction_to_target + self.GRAVITY)
+
+        return dive_acceleration
 
     def _check_phase_transition(self, current_position):
         """ Check if missile should transition from cruise to dive phase
@@ -183,16 +236,20 @@ class CruiseMissileTargetModel(TargetModel):
         :param time_step: Time step
         """
         self.velocity_ms = self.velocity_mach / MACH_2_MS
-        if self.current_phase == "cruise":
-            self._apply_disturbance(time_step)
-            self.target_position += self.velocity_ms * time_step
+        air_resistance = self._calculate_air_resistance()
 
+        if self.current_phase == "cruise":
+            control_acceleration = self._apply_cruise_control()
             if self._check_phase_transition(self.target_position):
                 self.current_phase = "dive"
-        elif self.current_phase == "dive":
-            total_acceleration = self.GRAVITY + np.array([0, 0, -self.rocket_acceleration])
-            self.velocity_ms += total_acceleration * time_step
-            self.target_position += self.velocity_ms * time_step
+        else:  # dive phase
+            control_acceleration = self._apply_dive_control()
+
+        self.acceleration = control_acceleration + air_resistance / MACH_2_MS
+        self.velocity_ms += self.acceleration * time_step
+        self.target_position += self.velocity_ms * time_step
+
+        self.velocity_mach = self.velocity_ms / MACH_2_MS
 
 
 class AircraftTargetModel(TargetModel):
@@ -206,9 +263,13 @@ class AircraftTargetModel(TargetModel):
         velocity_mach:      The aircraft target velocity (in Mach), inherited from the TargetModel class.
 
     """
-    PRIORITY = 2
+    PRIORITY = 3
     MIN_ALTITUDE = 5000
     MAX_ALTITUDE = 10000
+    GRAVITY = np.array([0, 0, -9.81])
+    AIR_RESISTANCE_COEF = 0.1
+    TURN_RATE_MAX = 0.1
+    VERTICAL_ACCELERATION = 5
 
     def __init__(self, target_id, target_position, velocity_mach):
         """ Initializes the aircraft target model.
@@ -222,6 +283,53 @@ class AircraftTargetModel(TargetModel):
         super().__init__(target_id, target_position, velocity_mach, "Aircraft", self.PRIORITY)
         self.min_altitude = self.MIN_ALTITUDE
         self.max_altitude = self.MAX_ALTITUDE
+        self.acceleration = np.zeros(3)
+        self.heading = np.random.uniform(0, 2 * np.pi)
+
+    def _calculate_air_resistance(self):
+        """ Calculate air resistance.
+
+        The air resistance is calculated based on the current velocity of the target.
+
+        :return: Air resistance
+        """
+        velocity_magnitude = np.linalg.norm(self.velocity_ms)
+        if velocity_magnitude > 0:
+            resistance = -self.AIR_RESISTANCE_COEF * velocity_magnitude * self.velocity_ms
+            return resistance
+        return np.zeros(3)
+
+    def _apply_altitude_control(self):
+        """ Apply altitude control to the aircraft target.
+
+        :return: Altitude control acceleration
+        """
+        height_margin = 200
+        height = self.target_position[2]
+
+        if height < self.MIN_ALTITUDE + height_margin:
+            return np.array([0, 0, self.VERTICAL_ACCELERATION])
+        elif height > self.MAX_ALTITUDE - height_margin:
+            return np.array([0, 0, -self.VERTICAL_ACCELERATION])
+        return np.zeros(3)
+
+    def _apply_maneuver(self, time_step):
+        """ Apply maneuver to the aircraft target.
+
+        :param time_step: Time step
+        :return: Maneuver acceleration
+        """
+        self.heading += np.random.uniform(-self.TURN_RATE_MAX, self.TURN_RATE_MAX)
+
+        direction = np.array([
+            np.cos(self.heading),
+            np.sin(self.heading),
+            0
+        ])
+
+        disturbance = np.random.normal(0, 0.3, 3) * time_step
+
+        return direction, disturbance
 
     def update_position(self, time_step):
         """Updates the target position based on the time step.
@@ -231,15 +339,17 @@ class AircraftTargetModel(TargetModel):
         :param time_step: Time step
         """
         self.velocity_ms = self.velocity_mach * MACH_2_MS
-        disturbance = np.random.normal(0, 0.5, 3) * time_step
-        self.velocity_ms += disturbance
+        air_resistance = self._calculate_air_resistance()
 
-        height_margin = 200
-        adjustment_acceleration = 5
+        direction, disturbance = self._apply_maneuver(time_step)
 
-        if self.target_position[2] < self.MIN_ALTITUDE + height_margin:
-            self.velocity_ms[2] += adjustment_acceleration * time_step
-        elif self.target_position[2] > self.MAX_ALTITUDE - height_margin:
-            self.velocity_ms[2] -= adjustment_acceleration * time_step
+        altitude_control = self._apply_altitude_control()
 
+        speed = np.linalg.norm(self.velocity_ms)
+        self.velocity_ms = speed * direction + disturbance
+        self.acceleration = altitude_control + air_resistance
+
+        self.velocity_ms += self.acceleration * time_step
         self.target_position += self.velocity_ms * time_step
+
+        self.velocity_mach = self.velocity_ms / MACH_2_MS
